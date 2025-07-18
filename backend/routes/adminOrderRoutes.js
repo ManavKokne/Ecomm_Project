942dbc3ec5f85a7bf29e56d91bfdc4831cb1188d
ManavKokne <koknemanav@gmail.com>
2025-07-18 18:22:01 +0530

    D13_Updates

---
+++ backend/routes/adminOrderRoutes.js
@@ -0,0 +1,80 @@
+const express = require("express")
+const Order = require("../models/Order")
+const {protect,admin} = require("../middleware/authMiddleware")
+
+const router = express.Router();
+
+// @route GET /api/admin/orders
+// @desc Get all the order (admin only)
+// @access Private/Admin
+
+router.get("/", protect,admin,async(req,res)=>{
+    try{
+        const orders = await Order.find({}).populate(
+            "user",
+            "name email"
+        )
+        res.json(orders);
+    }catch(err){
+        console.error(err);
+        res.status(500).json({
+            message : "Server Error"
+        })
+    }
+});
+
+// @route PUT /api/admin/orders/:id
+// @desc Update the order status
+// @access Private/Admin
+
+router.put("/:id",protect,admin,async(req,res)=>{
+    try{
+        const order = await Order.findById(req.params.id);
+
+        if(order){
+            order.status = req.body.status || order.status;
+            order.isDelivered = req.body.status === "Delivered" ? true : order.isDelivered;
+            order.deliveredAt = req.body.status === "Delivered" ? Date.now() : order.deliveredAt; 
+        
+            const updatedOrder = await order.save();
+            res.json(updatedOrder);
+        }else{
+            res.status(404).json({
+                message : "Order not found"
+            })
+        }
+    }catch(err){
+        console.error(err);
+        res.status(500).json({
+            message : "Server Error"
+        })
+
+    }
+});
+
+// @route DELETE /api/admin/orders/:id
+// @desc Delete an Order
+// @access Private/Admin
+
+router.delete("/:id",protect,admin,async(req,res)=>{
+    try{
+        const order = await Order.findById(req.params.id);
+        if(order){
+            await order.deleteOne();
+            res.json({
+                message : "Order Removed"
+            })
+        }else{
+            res.status(404).json({
+                message : "Order not Found"
+            })
+        }
+    }catch(err){
+        console.error(err);
+        res.status(500).json({
+            message : "Server Error"
+        })
+    }
+})
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,12 @@ const userRoutes = require("./routes/userRoutes")
 const productRoutes = require("./routes/productRoutes")
 const cartRoutes = require("./routes/cartRoutes")
 const checkoutRoutes = require("./routes/checkoutRoutes")
+const orderRoutes = require("./routes/orderRoutes")
+const uploadRoutes = require("./routes/uploadRoutes")
+const subscribeRoute = require("./routes/subscribeRoute")
+const adminRoutes = require("./routes/adminRoutes")
+const productAdminRoutes = require("./routes/productAdminRoutes")
+const adminOrderRoutes = require("./routes/adminOrderRoutes")
 
 const app = express(); //initialise express app
 app.use(express.json()); // ensures that server able to work with json data
@@ -27,6 +33,14 @@ app.use("/api/users", userRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/checkout", checkoutRoutes);
+app.use("/api/orders", orderRoutes);
+app.use("/api/upload", uploadRoutes);
+app.use("/api", subscribeRoute);
+
+//Admin Routes
+app.use("/api/admin/users",adminRoutes);
+app.use("/api/admin/products",productAdminRoutes);
+app.use("/api/admin/orders",adminOrderRoutes);
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on http://localhost:${PORT}`);
